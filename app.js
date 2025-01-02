a16a17302bdf9cb2705c9220eca9663af4fbdef6
varshapanda <varsha.vijayashwini@kalvium.community>
2025-01-02 15:00:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/signup',(req,res)=>{
+    const {userName,email,password, date_of_birth} = req.body;
+    
+
+    res.send('hello!');
+
+
+})
+
+const PORT = 3000;
+app.listen(PORT,()=>{
+    console.log(`Your server is running on port http://localhost:${PORT}`);
+})
